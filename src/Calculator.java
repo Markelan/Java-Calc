9e5fe1dc7968ae97f9004624c93197b82c9193ac
qoxx <garadazhov.maxim2003@yandex.ru>
2023-02-11 03:48:03 +0300

    структурировал объявления переменных, уменьшил кол-во строчек

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,94 +6,61 @@ public class Calculator {
     private JPanel Calculator;
     private JTextField txtDisplay;
     private JButton ACButton;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton a7Button;
-    private JButton a4Button;
+    private JButton addButton;
+    private JButton subButton;
+    private JButton multiButton;
+    private JButton divButton;
+    private JButton finalButton;
+    private JButton backspaceButton;
     private JButton a1Button;
-    private JButton a00Button;
-    private JButton button11;
-    private JButton a8Button;
-    private JButton a5Button;
     private JButton a2Button;
-    private JButton a0Button;
-    private JButton button16;
-    private JButton a9Button;
-    private JButton a6Button;
     private JButton a3Button;
-    private JButton button20;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton a0Button;
+    private JButton a00Button;
+    private JButton pmButton;
+    private JButton dotButton;
 
-    double a, b, result;
-    Operator op;
+    private double a;
+    private Operator o;
 
-    // pediki
+    private void step(double a, Operator o){
+        this.a = a;
+        this.o = o;
+        txtDisplay.setText("");
+    }
+
     public Calculator() {
         ACButton.addActionListener(e -> txtDisplay.setText(""));
-
-        a7Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a7Button.getText()));
-        a8Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a8Button.getText()));
         a9Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a9Button.getText()));
-        a4Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a4Button.getText()));
-        a5Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a5Button.getText()));
+        a8Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a8Button.getText()));
+        a7Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a7Button.getText()));
         a6Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a6Button.getText()));
-        a1Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a1Button.getText()));
-        a2Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a2Button.getText()));
+        a5Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a5Button.getText()));
+        a4Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a4Button.getText()));
         a3Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a3Button.getText()));
+        a2Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a2Button.getText()));
+        a1Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a1Button.getText()));
         a0Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a0Button.getText()));
         a00Button.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + a00Button.getText()));
-        button16.addActionListener(e -> {
-            if (txtDisplay.getText().contains(".")) {
-                double pm = Double.parseDouble(txtDisplay.getText());
-                pm = pm * -1;
-                txtDisplay.setText(String.valueOf(pm));
-            }
-            else {
-                long PM = Long.parseLong(txtDisplay.getText());
-                PM = PM * -1;
-                txtDisplay.setText(String.valueOf(PM));
-            }
-        });
-        button20.addActionListener(e -> {
-            if (!txtDisplay.getText().contains(".")) {
-                txtDisplay.setText(txtDisplay.getText() + button20.getText());
-            }
-        });
-        button11.addActionListener(e -> {
-            a = Double.parseDouble(txtDisplay.getText());
-            op = Add.get();
-            txtDisplay.setText("");
-        });
-        button2.addActionListener(e -> {
-            a = Double.parseDouble(txtDisplay.getText());
-            op = Subtract.get();
-            txtDisplay.setText("");
-        });
-        button3.addActionListener(e -> {
-            a = Double.parseDouble(txtDisplay.getText());
-            op = Multiply.get();
-            txtDisplay.setText("");
-        });
-        button4.addActionListener(e -> {
-            a = Double.parseDouble(txtDisplay.getText());
-            op = Divide.get();
-            txtDisplay.setText("");
-        });
-        button6.addActionListener(e -> {
-
+        pmButton.addActionListener(e -> txtDisplay.setText(String.valueOf((-1) * Double.parseDouble(txtDisplay.getText()))));
+        dotButton.addActionListener(e -> txtDisplay.setText(txtDisplay.getText() + (txtDisplay.getText().contains(".") ? "" : dotButton.getText())));
+        addButton.addActionListener(e -> this.step(Double.parseDouble(txtDisplay.getText()), Add.get()));
+        subButton.addActionListener(e -> this.step(Double.parseDouble(txtDisplay.getText()), Subtract.get()));
+        multiButton.addActionListener(e -> this.step(Double.parseDouble(txtDisplay.getText()), Multiply.get()));
+        divButton.addActionListener(e -> this.step(Double.parseDouble(txtDisplay.getText()), Divide.get()));
+        backspaceButton.addActionListener(e -> {
             if (txtDisplay.getText().length() > 0) {
                 StringBuilder strB = new StringBuilder(txtDisplay.getText());
-                strB.deleteCharAt(txtDisplay.getText().length() - 1);
-                txtDisplay.setText(String.valueOf(strB));
+                txtDisplay.setText(String.valueOf(strB.deleteCharAt(txtDisplay.getText().length() - 1)));
             }
         });
-        button5.addActionListener(e -> {
-            b = Double.parseDouble(txtDisplay.getText());
-            result = op.getResult(a, b);
-            txtDisplay.setText(String.valueOf(result));
-        });
+        finalButton.addActionListener(e -> txtDisplay.setText(String.valueOf(o.getResult(a, Double.parseDouble(txtDisplay.getText())))));
     }
 
 
